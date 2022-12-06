783b0a5eaad24495c18542152d2d385fc180cfe2
NICOLAS CASANOVA VASQUEZ <nicolas.casanova06@inacapmail.cl>
2022-12-06 13:43:30 -0300

    se agrego un ViewModel de la vista inicio.

---
+++ SistemaCajaRegistradora/Models/ViewModels/vmAmountItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaCajaRegistradora.Models.ViewModels
+{
+    public class vmAmountItems
+    {
+        public string amountProd { get; set; }
+        public string amountCategoria { get; set; }
+        public string totalVentas { get; set; }
+        public string amountUsuarios { get; set; }
+    }
+}
