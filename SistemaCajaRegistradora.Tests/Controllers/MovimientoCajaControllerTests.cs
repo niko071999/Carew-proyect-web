1486416536777e20fa93148f00e9a07cd9b9cf21
NICOLAS CASANOVA VASQUEZ <nicolas.casanova06@inacapmail.cl>
2022-12-06 13:45:06 -0300

    Se agregaron algunos testeo

---
+++ SistemaCajaRegistradora.Tests/Controllers/MovimientoCajaControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaCajaRegistradora.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCajaRegistradora.Controllers.Tests
+{
+    [TestClass]
+    public class MovimientoCajaControllerTests
+    {
+        [TestMethod]
+        public void verificarCajaUsoTest()
+        {
+            int id_entrada = 1;
+            MovimientoCajaController mc = new MovimientoCajaController();
+            bool result = mc.verificarCajaUso(id_entrada);
+
+            if (id_entrada > 0)
+            {
+                Assert.IsTrue(result);
+            }
+            else
+            {
+                Assert.Fail("ID <= 0");
+            }
+        }
+    }
+}
---
+++ SistemaCajaRegistradora.Tests/Controllers/ProductoControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using SistemaCajaRegistradora.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
+
+namespace SistemaCajaRegistradora.Controllers.Tests
+{
+    [TestClass]
+    public class ProductoControllerTest
+    {
+        [TestMethod]
+        public void ListarTest()
+        {
+            ProductoController pc = new ProductoController();
+            ViewResult r = pc.Listar() as ViewResult;
+
+            Assert.IsNotNull(r);
+        }
+    }
+}
